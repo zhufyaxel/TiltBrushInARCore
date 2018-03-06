43f11f02fefffcc437628563155a1c81d3139371
Zhu Fengyuan <zhufyaxel@gmail.com>
2018-03-06 17:35:09 -0500

    Initial commit

---
+++ Assets/Dev/Scripts/GameController.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using GoogleARCore;
+using GoogleARCore.HelloAR;
+
+public class GameController : MonoBehaviour {
+
+    public Camera FirstPersonCamera;
+    public GameObject TrackedPlanePrefab;
+    public GameObject YourArtWork;
+    public GameObject SearchingForPlaneUI;
+    private List<TrackedPlane> m_NewPlanes = new List<TrackedPlane>();
+    private List<TrackedPlane> m_AllPlanes = new List<TrackedPlane>();
+    private bool m_IsQuitting = false;
+    public void Start()
+    {
+        if (YourArtWork != null)
+        {
+            YourArtWork.SetActive(false);
+        }
+    }
+
+
+    public void Update()
+        {
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                Application.Quit();
+            }
+
+            _QuitOnConnectionErrors();
+
+            // Check that motion tracking is tracking.
+            if (Session.Status != SessionStatus.Tracking)
+            {
+                const int lostTrackingSleepTimeout = 15;
+                Screen.sleepTimeout = lostTrackingSleepTimeout;
+                if (!m_IsQuitting && Session.Status.IsValid())
+                {
+                    SearchingForPlaneUI.SetActive(true);
+                }
+
+                return;
+            }
+
+            Screen.sleepTimeout = SleepTimeout.NeverSleep;
+
+            // Iterate over planes found in this frame and instantiate corresponding GameObjects to visualize them.
+            Session.GetTrackables<TrackedPlane>(m_NewPlanes, TrackableQueryFilter.New);
+            for (int i = 0; i < m_NewPlanes.Count; i++)
+            {
+                // Instantiate a plane visualization prefab and set it to track the new plane. The transform is set to
+                // the origin with an identity rotation since the mesh for our prefab is updated in Unity World
+                // coordinates.
+                GameObject planeObject = Instantiate(TrackedPlanePrefab, Vector3.zero, Quaternion.identity,
+                    transform);
+                planeObject.GetComponent<TrackedPlaneVisualizer>().Initialize(m_NewPlanes[i]);
+            }
+
+            // Disable the snackbar UI when no planes are valid.
+            Session.GetTrackables<TrackedPlane>(m_AllPlanes);
+            bool showSearchingUI = true;
+            for (int i = 0; i < m_AllPlanes.Count; i++)
+            {
+                if (m_AllPlanes[i].TrackingState == TrackingState.Tracking)
+                {
+                    showSearchingUI = false;
+                    break;
+                }
+            }
+
+            SearchingForPlaneUI.SetActive(showSearchingUI);
+
+            // If the player has not touched the screen, we are done with this update.
+            Touch touch;
+            if (Input.touchCount < 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
+            {
+                return;
+            }
+
+            // Raycast against the location the player touched to search for planes.
+            TrackableHit hit;
+            TrackableHitFlags raycastFilter = TrackableHitFlags.PlaneWithinPolygon |
+                TrackableHitFlags.FeaturePointWithSurfaceNormal;
+
+            if (Frame.Raycast(touch.position.x, touch.position.y, raycastFilter, out hit))
+            {
+                YourArtWork.SetActive(true);
+                Vector3 newPose = hit.Pose.position;
+                YourArtWork.transform.position = newPose;
+                newPose.x = FirstPersonCamera.transform.position.x;
+                newPose.z = FirstPersonCamera.transform.position.z;
+                YourArtWork.transform.LookAt(newPose);
+            
+                var anchor = hit.Trackable.CreateAnchor(hit.Pose);
+
+                YourArtWork.transform.parent = anchor.transform;
+            }
+        }
+
+        /// <summary>
+        /// Quit the application if there was a connection error for the ARCore session.
+        /// </summary>
+        private void _QuitOnConnectionErrors()
+        {
+            if (m_IsQuitting)
+            {
+                return;
+            }
+
+            // Quit if ARCore was unable to connect and give Unity some time for the toast to appear.
+            if (Session.Status == SessionStatus.ErrorPermissionNotGranted)
+            {
+                _ShowAndroidToastMessage("Camera permission is needed to run this application.");
+                m_IsQuitting = true;
+                Invoke("_DoQuit", 0.5f);
+            }
+            else if (Session.Status.IsError())
+            {
+                _ShowAndroidToastMessage("ARCore encountered a problem connecting.  Please start the app again.");
+                m_IsQuitting = true;
+                Invoke("_DoQuit", 0.5f);
+            }
+        }
+
+        /// <summary>
+        /// Actually quit the application.
+        /// </summary>
+        private void _DoQuit()
+        {
+            Application.Quit();
+        }
+
+        /// <summary>
+        /// Show an Android toast message.
+        /// </summary>
+        /// <param name="message">Message string to show in the toast.</param>
+        private void _ShowAndroidToastMessage(string message)
+        {
+            AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+            AndroidJavaObject unityActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+
+            if (unityActivity != null)
+            {
+                AndroidJavaClass toastClass = new AndroidJavaClass("android.widget.Toast");
+                unityActivity.Call("runOnUiThread", new AndroidJavaRunnable(() =>
+                {
+                    AndroidJavaObject toastObject = toastClass.CallStatic<AndroidJavaObject>("makeText", unityActivity,
+                        message, 0);
+                    toastObject.Call("show");
+                }));
+            }
+        }
+}
+
+
